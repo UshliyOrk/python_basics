bd8197f5c6140ca3c1d94196841cd253876e2a84
UshliyOrk <codernamedpaul@outlook.com>
2023-01-22 01:30:15 +0300

    задания(1,2 ГЛАВЫ)

---
+++ задания/глава1/параграф4/задание2.py
@@ -0,0 +1,10 @@
+"""
+1. выведите 6 символ и 9 с конца
+2. выедите все сиволы с 4 по 10
+3. выедите длинну строки
+4. выведите срез всей строки, но с шагом 5 (начало:конец:шаг)
+"""
+
+s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+
+
---
+++ задания/глава1/параграф4/задание3.py
@@ -0,0 +1,10 @@
+"""
+1. найдите первую и последнюю запятые(индекс)
+2. разбейте строку по пробелам
+3. найдите индекс подстроки "sed"
+4. замените запятые на звездочки
+5. доплните строку любым сиволом до длинны 130, сначала справа. а потом слева
+"""
+
+s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+
