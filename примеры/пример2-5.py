6d5afbd9dda0e592278148eecc32042d1cd330ee
UshliyOrk <codernamedpaul@outlook.com>
2023-01-21 23:42:20 +0300

    правки

--- примеры/пример2-5.py
+++ примеры/пример2-5.py
@@ -1,4 +1,6 @@
 print("Hello world!", 10)
+s = input("Введите текст: ")  # запрашиваем строку
+print(s)  # выводим
 print('Hello\nworld!')
 print('Hello,','Brad' , 'and Vlad', sep='=')
 print('Hello,','Bard' , 'and Vad', end='novaya stroka')
